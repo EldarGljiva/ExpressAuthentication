768ac81628b7eecdae89a61c871186c9c76ead68
Eldar Gljiva <=eldar.gljiva@stu.ibu.edu.ba>
2023-12-14 16:22:02 +0100

    authentication with google OAuth

---
+++ googleOAUTH.js
@@ -0,0 +1,176 @@
+import bodyParser from "body-parser";
+import express from "express";
+import mongoose from "mongoose";
+import session from "express-session";
+import passport from "passport";
+import passportLocalMongoose from "passport-local-mongoose";
+import LocalStrategy from "passport-local";
+import GoogleStrategy from "passport-google-oauth20";
+GoogleStrategy.Strategy;
+import findOrCreate from "mongoose-findorcreate";
+import dotenv from "dotenv";
+dotenv.config();
+//Passport is middleware for Node.js that makes it easy to implement authentication and authorization.
+const app = express();
+const port = 3000;
+
+//middleware
+app.use(express.static("public")); //use public folder as static files
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.use(
+  session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize()); //initialize passport
+app.use(passport.session()); //manage passport to use session
+
+mongoose
+  .connect("mongodb://localhost:27017/userDB")
+  .then(() => console.log("Database connected succesfully"))
+  .catch((err) => {
+    console.log("Database connection failed, " + err);
+  });
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  googleId: String,
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = mongoose.model("User", userSchema);
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    User.authenticate()
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+  User.findById(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((err) => {
+      done(err, null);
+    });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/content",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
+
+app.get("/", async (req, res) => {
+  try {
+    res.render("home.ejs");
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get(
+  "/auth/google/content",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    console.log("Google authentication callback hit");
+    res.redirect("/content");
+  }
+);
+
+app.get("/login", async (req, res) => {
+  try {
+    res.render("login.ejs");
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.get("/register", async (req, res) => {
+  try {
+    res.render("register.ejs");
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.get("/logout", async (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/");
+  });
+});
+
+app.get("/content", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("content");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/register", async (req, res) => {
+  User.register(
+    { username: req.body.email },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        console.log("User registered: " + user);
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/content");
+        });
+      }
+    }
+  );
+});
+app.post("/login", async (req, res) => {
+  const user = new User({
+    username: req.body.email,
+    password: req.body.password,
+  });
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      //local because local user database
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/content");
+      });
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Application is listening on port ${port}`);
+});
